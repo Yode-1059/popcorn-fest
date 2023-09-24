8af7bc1a80cdc381341fe118431c35fb814a7069
Yode-1059 <yoshinaga.web@gmail.com>
2023-09-24 12:04:26 +0900

    dsafsv

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -13,18 +13,6 @@ const PostItem: React.FC<any> = ({ post }) => {
     setIsModalOpen(false);
   };
 
-  const isVideoFile = (url: string) => {
-    try {
-      const videoExtensions = ['mp4', 'webm', 'ogg', 'mov'];
-      const urlObject = new URL(url);
-      const extension = urlObject.pathname.split('.').pop()?.toLowerCase();
-      return extension ? videoExtensions.includes(extension) : false;
-    } catch (e) {
-      console.error('Invalid URL', e);
-      return false;
-    }
-  };
-
   return (
     <div>
       {post ? (
